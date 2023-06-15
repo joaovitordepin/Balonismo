040dca108eca222bd74613a6b86e1d91ef85afab
joaovitordepin <ceocareoficial@gmail.com>
2023-06-15 12:30:29 -0300

    Alteração 15/06/23

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,29 @@ public class Main {
 
         Pessoa p1 = new Pessoa();
 
-        Pessoa p2; // declaração do objeto
-        p2 = new Pessoa(); // construção
+        Pessoa p2; new Pessoa();
+        Pessoa p3; new Pessoa();// declaração do objeto
+         // construção
+
+
+
+
         //...
         p1.nome = "Elliot Page";
+        p1.cep = "06414-030";
+        p1.rg = "11.111.111-1";
+        p1.cpf = "555.555.555.55";
+        p1.tel = "(11) 96675-9070";
+        p1.telfami = "(11) 98744-0931";
+
+        p2.nome = "Maria";
+        p2.cep = "90414-021";
+        p2.rg = "11.126.751-1";
+        p2.cpf = "585.115.505.58";
+        p2.tel = "(11) 96552-9780";
+        p2.telfami = "(11) 98712-0939";
+
+        p3.nome = "Piloto";
+        p3.cpf = "422.037.690.00";
     }
 }
--- src/Pessoa.java
+++ src/Pessoa.java
@@ -5,6 +5,18 @@ public class Pessoa {
 
     String nome;
 
+    String cep;
+
+    String cpf; //111.111.111.11
+
+    String rg;
+
+    String tel;
+
+    String telfami;
+
+    String proAnac;
+
     //Métodos
     public void pesquisarEmpresa(){
 
